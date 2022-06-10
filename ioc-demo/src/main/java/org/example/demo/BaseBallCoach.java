92ec4d4f94cfa6a7f07dfcb66d547ae9250a0a9e
Gurmeet Singh <76160628+1810991772@users.noreply.github.com>
2022-06-10 10:02:06 +0530

    Add files via upload

---
+++ ioc-demo/src/main/java/org/example/demo/BaseBallCoach.java
@@ -0,0 +1,11 @@
+package org.example.demo;
+
+public class BaseBallCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return ("Work Hard Today");
+	}
+
+}
---
+++ ioc-demo/src/main/java/org/example/demo/CricketCoach.java
@@ -0,0 +1,11 @@
+package org.example.demo;
+
+public class CricketCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return ("Practice Batting Today");
+	}
+
+}
